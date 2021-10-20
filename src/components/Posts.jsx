80f712abbd7d95a7d443ca2eb3c4d1bc55d2442a
Nnabue Favour Chukwuemeka <favour.chukwuemeka@decagon.dev>
2021-10-21 00:31:37 +0100

    Update Posts.jsx

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -11,7 +11,7 @@ const Posts = () => {
   const handleFecth = async () => {
     try {
       setIsLoading(true);
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const response = await fetch(url)
     const data = await response.json();
     setAllPosts(data);
     console.log(data)
